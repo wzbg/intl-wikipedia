7b06315b6d7f8254562e4580a25923f0ef7893e5
zyc <z35305515@gmail.com>
2016-02-19 11:15:33 +0800

    decodeURI page name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: zyc
 * @Date:   2016-02-18 19:42:54
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-02-19 01:47:09
+* @Last Modified time: 2016-02-19 11:14:21
 */
 'use strict'
 
@@ -10,12 +10,14 @@ const fetchUrl = require('fetch').fetchUrl
 const easypedia = require('easypedia')
 
 module.exports = (searchTerm, language) => {
+  searchTerm = encodeURI(searchTerm)
   return new Promise((resolve, reject) => {
     easypedia(searchTerm, { language }, (error, page) => {
       if (error) return reject(error)
+      page.name = decodeURI(page.name)
       const text = page._text || {}
       if (Object.keys(text).length) return resolve(page)
-      const url = `https://${page.language}.wikipedia.org/w/api.php?action=query&list=search&utf8&format=json&srsearch=${encodeURI(searchTerm)}`
+      const url = `https://${page.language}.wikipedia.org/w/api.php?action=query&list=search&utf8&format=json&srsearch=${searchTerm}`
       fetchUrl(url, (err, res, buf) => {
         if (err || res.status != 200) return resolve(page)
         const results = JSON.parse(buf).query.search
@@ -32,6 +34,7 @@ const easypedias = (searchTerms, language, index) => {
     if (searchTerms.length === index) return reject(new Error(`error index: ${index}`))
     easypedia(encodeURI(searchTerms[index]), { language }, (error, page) => {
       if (error) return easypedias(searchTerms, language, ++index)
+      page.name = decodeURI(page.name)
       resolve(page)
     })
   })
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 * @Author: zyc
 * @Date:   2016-02-18 14:39:14
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-02-19 01:01:11
+* @Last Modified time: 2016-02-19 11:14:42
 */
 'use strict'
 
